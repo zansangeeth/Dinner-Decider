4b914c9d54c69ec70f8b754517f0f34dd3857537
zansangeeth <zansangeeth17@gmail.com>
2021-08-25 13:31:14 +0530

    updated edit text, store the values on array and show randomely

--- app/src/main/java/com/zasa/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/zasa/dinnerdecider/MainActivity.kt
@@ -2,11 +2,27 @@ package com.zasa.dinnerdecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList  = arrayListOf("Mc donalds", "KFC, burger", "king", "Srilankan food")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            selectFoodText.setText(foodList.random())
+        }
+
+        addFoodBtn.setOnClickListener {
+            foodList.add(addFoodTxt.text.toString())
+        }
+
+
     }
 
 
