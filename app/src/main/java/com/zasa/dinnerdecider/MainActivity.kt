bc951298f0036fd13980edcfbc634989eb1a3ffe
zansangeeth <zansangeeth17@gmail.com>
2021-08-25 15:35:59 +0530

    alternative way of develop and formatted code

--- app/src/main/java/com/zasa/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/zasa/dinnerdecider/MainActivity.kt
@@ -2,25 +2,44 @@ package com.zasa.dinnerdecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    val foodList  = arrayListOf("Mc donalds", "KFC, burger", "king", "Srilankan food")
+    val foodList = arrayListOf("Mc donalds", "KFC", "burger", "king", "Srilankan food")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         decideBtn.setOnClickListener {
-            selectFoodText.setText(foodList.random())
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectFoodText.text = foodList[randomFood]
         }
 
         addFoodBtn.setOnClickListener {
             foodList.add(addFoodTxt.text.toString())
+            addFoodTxt.text.clear()
+
+            Log.i("listOfFoods", foodList.toString())
         }
+
+
+
+        //alternate way to implement dinnerDecider
+        
+//        decideBtn.setOnClickListener {
+//            selectFoodText.setText(foodList.random())
+//        }
+//
+//        addFoodBtn.setOnClickListener {
+//            foodList.add(addFoodTxt.text.toString())
+//        }
 
 
     }
